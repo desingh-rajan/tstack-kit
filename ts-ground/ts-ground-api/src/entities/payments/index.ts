6bc688ffa1051bd9c256d9297c4c0ff986e7f49e
Desingh <desinghrajan@gmail.com>
2026-01-03 01:08:08 +0530

    chore: untrack ts-ground directory (local dev/test workspace)

--- ts-ground/ts-ground-api/src/entities/payments/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Payments Module Exports
- */
-
-export * from "./payment.model.ts";
-export * from "./payment.dto.ts";
-export * from "./payment.service.ts";
-export * from "./payment.controller.ts";
